c3e117e96d176874fe96c929c3ec1ee6f9212952
Stalin <p.zamorskiy@yandex.ru>
2022-05-28 05:47:41 +0300

    Project init

---
+++ analyzer/__init__.py
@@ -0,0 +1,19 @@
+"""
+Сервис с REST API, анализирующий рынок для промо-акций.
+"""
+
+__author__ = "Peter 'stalingd' Zamorskiy"
+__maintainer__ = __author__
+
+__email__ = 'p.zamorkiy@yandex.ru'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
